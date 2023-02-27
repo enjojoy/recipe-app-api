4120dfb4363d5fc16772fa10905ddddd0a4db4bf
enjojoy <albina.nikiforova.5670@gmail.com>
2023-02-27 08:58:08 +0100

    Checks waits for db

--- app/app/tests.py
+++ app/app/tests.py
@@ -6,9 +6,10 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """Test the calc module"""
-    
+
     def test_add_numbers(self):
         """Test adding numbers together"""
         res = calc.add(5, 6)
@@ -20,6 +21,3 @@ class CalcTests(SimpleTestCase):
         res = calc.subtract(10, 15)
 
         self.assertEqual(res, 5)
-
-    
-
